61b66da45a99aaa273e46c2f4a52b788e4cea749
ShenJack <357875929@qq.com>
2018-03-18 09:45:18 +0800

    4

--- answer.js
+++ answer.js
@@ -2,6 +2,7 @@
 var pages = new Map();
 pages.add();
 var count = 1;
+var content = '';
 for (var i = 1; i < 250;i++) {
     $.post('postDrag.php', {
         ItemID: i
@@ -14,5 +15,22 @@ for (var i = 1; i < 250;i++) {
             console.log(' : ' + data);
             count++;
         }
+    }).error(function () {
+        console.log('error');
     })
 }
+
+var funDownload = function (content, filename) {
+    // 创建隐藏的可下载链接
+    var eleLink = document.createElement('a');
+    eleLink.download = filename;
+    eleLink.style.display = 'none';
+    // 字符内容转变成blob地址
+    var blob = new Blob([content]);
+    eleLink.href = URL.createObjectURL(blob);
+    // 触发点击
+    document.body.appendChild(eleLink);
+    eleLink.click();
+    // 然后移除
+    document.body.removeChild(eleLink);
+};
--- autounipus.py
+++ autounipus.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path='C:\Gaming\chromedriver.exe')
+driver = webdriver.Chrome(executable_path='C:\chromedriver.exe')
 
 username = 161002516
 password = 161002516
@@ -11,9 +11,28 @@ driver.find_element_by_id("username").send_keys(username)
 driver.find_element_by_id("password").send_keys(password)
 driver.find_element_by_class_name("newlogintable_third_button").click()
 
+driver.get('http://202.204.121.159/book/index.php?BookID=184&ClassID=9089&Quiz=N')
+driver.get('http://202.204.121.159/book/book184/app_index.php?unit=1')
+
 my_script = open("answer.js", encoding='utf-8').read()
 # addIn = "var keyScore = %s; \n" % (80 / 100)
 
-my_script = addIn + my_script
-
-driver.execute_script(my_script)
+for j in range(0,180):
+    result = driver.execute_script('''
+    var count = 1;
+    var content = '';
+    $.post('postDrag.php', {
+        ItemID: arguments[0],
+    }, function (data) {
+        var question = JSON.parse(data);
+        if(question.key!==''){
+            return ('------- '+count + ' ------- ' + ' : ' + data);
+            count++;
+        }else{
+            return ('none error');
+        }
+    }).error(function () {
+        return ('error');
+    })
+''',j)
+    print(result)
