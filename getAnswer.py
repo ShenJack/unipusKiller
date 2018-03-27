a914369b7a787c63489e0fa2ac29fe693e3975dc
ShenJack <357875929@qq.com>
2018-03-27 17:13:55 +0800

    8

--- getAnswer.py
+++ getAnswer.py
@@ -6,7 +6,7 @@ chromeOptions = webdriver.ChromeOptions()
 prefs = {"profile.managed_default_content_settings.images": 2}
 chromeOptions.add_experimental_option("prefs", prefs)
 
-driver = webdriver.Chrome(executable_path='C:\Gaming\chromedriver.exe',options=chromeOptions)
+driver = webdriver.Chrome(executable_path='C:\chromedriver.exe', options=chromeOptions)
 
 username = 171002112
 password = 111111
@@ -21,21 +21,25 @@ driver.get('http://202.204.121.159/book/index.php?BookID=182&ClassID=9016&Quiz=N
 driver.get('http://202.204.121.159/book/book182/app_index.php?unit=1')
 
 a = KeyGenerator()
-list = a.getKeys('大一下')
+# list = a.getKeys()
 
 percent = 80
 ran = Random()
 
-for (i, j) in list:
+answer = []
+keys = []
+
+for i in range(1, 190):
     form_data = ''
-    form_data +='ItemID=' + i
-    for key in j.split('^'):
-        if ran.random() > percent/100:
-            key = ''
-        form_data+='&answer[]=' + key
+    form_data += 'ItemID=' + str(i)
+    # for key in j.split('^'):
+    #     if ran.random() > percent / 100:
+    #         key = ''
+    #     form_data += '&answer[]=' + key
     result = driver.execute_script('''
     var done = false;
     var score = 0;
+    var answer = '';
     $.ajaxSetup({
                 async : false
             });
@@ -48,17 +52,27 @@ for (i, j) in list:
             console.log(question.myPercentScore);
             score =  question.myPercentScore;
             done = true;
+            answer = question.key;
         }else{
+            answer = '';
             done = true;
         }
     }).error(function () {
         console.log('error');
         done = true;
         return 'error';
+        answer = '';
     })
     while(!done){
-        
+
     }
-    return score;
-    ''',form_data)
+    return answer;
+    ''', form_data)
     print(result)
+    if result != '':
+        answer.append(result)
+        keys.append(str(i))
+
+symbol = '*'
+print(symbol.join(answer))
+print(symbol.join(keys))
--- keysProcess.py
+++ keysProcess.py
@@ -1,37 +1,59 @@
+class KeyGenerator:
+    keys184 = '''1^2^3^4*interview techniques^lead^in^looking for^boils down*professional job coach^doing research^practiced with|practised with^present yourself^interviewer^presentation and understanding^in relationship to the job^some examples^dealing with problems*1^3^5^7^8^9^10^11*B^C^A^C*C^A^D*7^8^18^20^29^30*B^D^A^D^C*open-necked|open|open necked^first^five|5^twice^pension^work longer^one|1^not paid^60|sixty^company car*D^B^A^C^A^B^C^D^A^D^B^D^B^C*3^4^6*undergoing a fundamental change^600|six hundred|six-hundred^consuming content^get it edited^click of a few buttons^earning money^for an author^never really catch on*B^D^A^D*C^B^A*20|twenty^once^ten|10^discussion^travel writing*D^A^C^B*B^G^E^A^D^F^C*literary specialist^close links^well-known writers|well known writers^the memorial^literary history^recent series of films^the home of^successful films^English-speaking world^contributes to*C^A^D^B^B^D^A^A^C^A^B^A^B^C*B^B^C^A^D*H^C^G^F^D^A^E^B*D^B^A*B^C^A*B^C^A^B*the leading designer^the clothes^a symbol^with a shoulder strap^integrated with^casual look^lasts and evolves^surpasses*D^A^A^C^B^B^C^D^D^B^C^D^A^B*4^6^7*B^A^C^C^B*E^F^B^A^C^G^D*not happy^make all of that better^make more choices^thinks about money^of having the money^would get solved^gives us more choices*D^B^D^C*D^B^C*we exchanged things^exchange don't last^a lot easier to use^then appeared^producing them^a big advantage of^has a certain value^exchanged the grain^a certain amount of silver^1870 to 1915|1870-1915*C^E^B^G^F^A^D*B^A^A^A^C*B^C^D^A^B^C^A^B^C^B^A^B^A^B*B^D^D^C^C*economic and political sectors^division of^teaching and nursing^make their own money^in the number of^looked after^far from perfect^it will be difficult^who might be able to^been responsible for^less well paid than men^are rewarded*A^B^B*C^A^D^C*3^5^6^7^17^18^22^26^30*D^F^B^C^E^A*the result of upbringing^learn to speak earlier^intelligence tests^are better at language skills^less interested in people^have better social skills^quite differently*C^B^A^C^D^D^A^B^C^B^C^C^B^D*E^F^C^A^H^G^D^B*no more than 170 km|no more than one hundred and seventy km^more than nine million|more than 9 million^Liverpool and Southampton^five or six|5 or 6|five/six|5/6^1912^over 1500|over 1,500|over one thousand and five hundred^five or six|5 or 6|five/six|5/6^more than one and a half million|more than 1.5 million*A^C^B^D*the biggest navy in the world^what it was like^brought tea^to put up the sails^passenger ships^in maritime history^the world's biggest cruise ship^five or six hours|5 or 6 hours|5/6 hours^furniture and toys*C^D^A*B^D^B*F*1533^1536^1558^1568^1587^1603*B^B^C^D^D^D*Pompeii^a guide to the town^almost 2000 years ago|almost 2,000 years ago|almost two thousand years ago^August^two days|2 days^two|2*turn around^erupt^praying^1748^two owners|2 owners^money^mirror*C^D^A^B^C^C^A^C^B^C^A^A^B^D*1^2^4^7^10^11^12^14*A^B^A^C^A^D*the third largest|the 3rd largest^1871^curtains of glass^15 years|fifteen years^70 seconds|seventy seconds^840 flights|eight hundred and forty flights|eight hundred-forty flights^29 miles|twenty nine miles|twenty-nine miles^a wonderful sports history*B^C^D*D^B^A^D*2^4^5^6*the second largest|the 2nd largest^glass and steel^light and heat^late December 2007^French architect^the amount of sunlight^glass curtain^the gardens and trees*greatest architects^bones|Bones^The Dancing House^Two|2|two^Thirty-three years|33 years|thirty three years|Thirty three years^billowing sail^1997^stucco|Stucco*is only^of^that^a^is the^which^the^at^of the^and*A^C^C^B^A^C^D^C^B^B^A^B^B^D*Exciting jobs,right^under 30|under thirty^your diet and your nutrition^making a lot of money^passionate about windsurfing^looking at the wind^going to prepare^that magical balance^not to be afraid*1^3^5^7^8^10^12*A^B^A^C^C*B^D^C*B^A^C*62|sixty two|sixty-two^an island^to be taken back home^in surprisingly good health^he had no family^successful businessman^800|eight hundred*mast^rudder^floated^repair^Hawaii^catching^drinking^old newspapers^watched videos^car accident^scared*B^D^A^A^B*makes a man^a lot more to it^need for recognition^fearless^born that way*C^B^B^B^D^A^B^C^B^C^D^B^C^A'''
+    keys182 = '''3^5*one of the best^talented students^well-known|well known^open doors^good social life^want it to be^on another campus^fun place^go to concerts^during the week*library system online^three|3^four|4^Brian^leisure purposes^the libraries*B^C^D*C^A*1^11^12^22^26^33*C^E^A^H^B^G^D^F*C^D^E^B^A*0^1^2^5^6^9^11^12^15^16^18^19*B^D^D^B^A*C^A^B^D^C^C^D^B^B^D^C^B^C^A*1^4^8^9^14^16^18^22*2^3^5*great EQ^importance^socially^get along with^useful^IQ scores^came up^deal with people^a lot more^so much faster^listen to them^understand and communicate*A^C*B^D*1^8^23^28^33^44^49^57^64^74^80^94^96^106^115^125*E^D^F^A^C^B*1^10^20^25^33^35^44*C^E^H^A^B^D^G^F*D^C^C^D^C^C^D^B^A^D^B^B^D^A*3|three|Three^5,000|five thousand|5000|Five thousand^800|eight hundred|Eight hundred^Cycling|cycling^gold|Gold*first^M|m^signature^affect^tears*crashed out^lift gold^sent female athletes^a defining moment^coming third^coming second^taking part*B^C*A^C*a sports film|sports film|sports films^a true story|true story|true stories^falls^breaks his leg|breaks his right leg^interviews*B^C^D^D*B*C^G^F^B^A^D^E*C^D^A^B^C^B^C^A^C^A^C^B^B^D*1^3^5*black-market industry|black market industry^filmed^reach a record high^this year alone^in the form of packages^a huge trade^hitting*2.5 million^10^4,000 kilos^93^300^tortoises and turtles*B^B*A^D*F^D^E^B^C^A*2^4*Anna Black^just over a week ago|Just over a week ago^about seven|about 7|About seven|About 7^male|man|Male|Man^mobile phone|cell phone|Mobile phone|Cell phone^2 men|Two men|two men^5 minutes|five minutes|Five minutes*B^A^A^B^D*D^B^A^C^B^C^D^B^B^C^D^C^D^C*1^3^5^6^7^8^12^13^17^20^21^25^26^27^28^29^30^31*E^A^C^D^B*E^C^F^B^D^A*careful negotiation^favourite movie|favorite movie^You're so sweet|You are so sweet^too many times^there's nothing on|there is nothing on^putting up with^watch the basketball game^was looking for*B^D*C^D*music^exhibitions^movies^music^classical music^movies^movies*Friday^<i>The House of the Flying Daggers</i>|The House of the Flying Daggers^7 pm|seven pm|Seven pm^12|twelve^Globe Cinema^Friday^The Art of Venice^10 am|ten am^free^George Inn^10 pm|ten pm|Ten pm^20|twenty*ice hockey^bowling^television^jogging^book clubs^volunteering^dancing^skiing^films^association^movies^tennis^surfing^aboriginal^bushwalking*A^B^K^I^G^E^D^H^F^C^J*B^D^B^C^A^D^C^D^D^C^B^B^A^D*1^4^7^10^11^13^14^15^19^23^24*A^D^C^A^D*areas of life^in our homes^computer-controlled^adjust the temperature^give new instructions^react to match^TV and Internet^outside changes^sun from entering^turn the oven on*A^C^B*B^A*2^5*dunking biscuits^held together^absorb liquid^travel through^hold the biscuit^producing a table*0^2^5^6^9^10^13^14*B^B^D^A^C^B*B^A^D^C^A^C^B^A^C^C^B^C^A^C*E^A^F^C^B^D*dawn service^remember^forget^died in battle^identified^one unshakable truth^conflicts^around the world^remembering^go home*A^B^C^A*C^B*B^D*businessman^11 years old|eleven years old^first name^his second name|his 2nd name^the same age as^terrible four years|terrible 4 years^survives^his Japanese friend^strength^courage*B^D^A^D^B*agriculture^No^although^transport^tracking^support*physical^uniforms^fighter station^bombed^contributed*B^C^D^A^B^D^A^C^C^A^B^D^B^C*D^D^B^B^A*a gap year^Communities in need^arranges for^taking part in^protective environment^breed and feed^build a storehouse^a survival activity*A^B*B^C*1^2^4^7^8*B^A^E^D^C^H^F^G*3^7^10^12^17^23*3^4^9^10*A^D^C^A*B^A^D^A^C^D^B^A^D^C^B^C^B^A'''
 
-class keyGenerator:
-    keys = '''1^2^3^4*interview techniques^lead^in^looking for^boils down*professional job coach^doing research^practiced with|practised with^present yourself^interviewer^presentation and understanding^in relationship to the job^some examples^dealing with problems*1^3^5^7^8^9^10^11*B^C^A^C*C^A^D*7^8^18^20^29^30*B^D^A^D^C*open-necked|open|open necked^first^five|5^twice^pension^work longer^one|1^not paid^60|sixty^company car*D^B^A^C^A^B^C^D^A^D^B^D^B^C*3^4^6*undergoing a fundamental change^600|six hundred|six-hundred^consuming content^get it edited^click of a few buttons^earning money^for an author^never really catch on*B^D^A^D*C^B^A*20|twenty^once^ten|10^discussion^travel writing*D^A^C^B*B^G^E^A^D^F^C*literary specialist^close links^well-known writers|well known writers^the memorial^literary history^recent series of films^the home of^successful films^English-speaking world^contributes to*C^A^D^B^B^D^A^A^C^A^B^A^B^C*B^B^C^A^D*H^C^G^F^D^A^E^B*D^B^A*B^C^A*B^C^A^B*the leading designer^the clothes^a symbol^with a shoulder strap^integrated with^casual look^lasts and evolves^surpasses*D^A^A^C^B^B^C^D^D^B^C^D^A^B*4^6^7*B^A^C^C^B*E^F^B^A^C^G^D*not happy^make all of that better^make more choices^thinks about money^of having the money^would get solved^gives us more choices*D^B^D^C*D^B^C*we exchanged things^exchange don't last^a lot easier to use^then appeared^producing them^a big advantage of^has a certain value^exchanged the grain^a certain amount of silver^1870 to 1915|1870-1915*C^E^B^G^F^A^D*B^A^A^A^C*B^C^D^A^B^C^A^B^C^B^A^B^A^B*B^D^D^C^C*economic and political sectors^division of^teaching and nursing^make their own money^in the number of^looked after^far from perfect^it will be difficult^who might be able to^been responsible for^less well paid than men^are rewarded*A^B^B*C^A^D^C*3^5^6^7^17^18^22^26^30*D^F^B^C^E^A*the result of upbringing^learn to speak earlier^intelligence tests^are better at language skills^less interested in people^have better social skills^quite differently*C^B^A^C^D^D^A^B^C^B^C^C^B^D*E^F^C^A^H^G^D^B*no more than 170 km|no more than one hundred and seventy km^more than nine million|more than 9 million^Liverpool and Southampton^five or six|5 or 6|five/six|5/6^1912^over 1500|over 1,500|over one thousand and five hundred^five or six|5 or 6|five/six|5/6^more than one and a half million|more than 1.5 million*A^C^B^D*the biggest navy in the world^what it was like^brought tea^to put up the sails^passenger ships^in maritime history^the world's biggest cruise ship^five or six hours|5 or 6 hours|5/6 hours^furniture and toys*C^D^A*B^D^B*F*1533^1536^1558^1568^1587^1603*B^B^C^D^D^D*Pompeii^a guide to the town^almost 2000 years ago|almost 2,000 years ago|almost two thousand years ago^August^two days|2 days^two|2*turn around^erupt^praying^1748^two owners|2 owners^money^mirror*C^D^A^B^C^C^A^C^B^C^A^A^B^D*1^2^4^7^10^11^12^14*A^B^A^C^A^D*the third largest|the 3rd largest^1871^curtains of glass^15 years|fifteen years^70 seconds|seventy seconds^840 flights|eight hundred and forty flights|eight hundred-forty flights^29 miles|twenty nine miles|twenty-nine miles^a wonderful sports history*B^C^D*D^B^A^D*2^4^5^6*the second largest|the 2nd largest^glass and steel^light and heat^late December 2007^French architect^the amount of sunlight^glass curtain^the gardens and trees*greatest architects^bones|Bones^The Dancing House^Two|2|two^Thirty-three years|33 years|thirty three years|Thirty three years^billowing sail^1997^stucco|Stucco*is only^of^that^a^is the^which^the^at^of the^and*A^C^C^B^A^C^D^C^B^B^A^B^B^D*Exciting jobs,right^under 30|under thirty^your diet and your nutrition^making a lot of money^passionate about windsurfing^looking at the wind^going to prepare^that magical balance^not to be afraid*1^3^5^7^8^10^12*A^B^A^C^C*B^D^C*B^A^C*62|sixty two|sixty-two^an island^to be taken back home^in surprisingly good health^he had no family^successful businessman^800|eight hundred*mast^rudder^floated^repair^Hawaii^catching^drinking^old newspapers^watched videos^car accident^scared*B^D^A^A^B*makes a man^a lot more to it^need for recognition^fearless^born that way*C^B^B^B^D^A^B^C^B^C^D^B^C^A'''
+    ids184 = '''6*7*8*9*11*12*13*14*15*23*29*31*32*33*34*35*36*37*43*50*51*53*54*56*57*65*71*72*73*74*75*76*78*79*80*87*93*94*96*97*98*100*101*108*114*115*116*117*119*120*121*122*123*124*125*132*138*139*140*141*142*144*145*146*153*154*160*161*162*164*165*166*167*168*169*176'''
+    ids182 = '''6*7*8*9*10*11*12*13*14*15*21*27*28*29*30*31*32*33*34*35*41*47*48*49*50*51*52*53*54*55*61*67*68*69*70*71*72*73*74*75*83*89*90*91*92*93*94*95*96*97*99*104*110*111*112*114*115*116*117*118*119*126*132*133*134*135*136*137*138*139*140*147*154*155*157*158*159*160*161*162*163*169'''
 
-    ids = '''6*7*8*9*11*12*13*14*15*23*29*31*32*33*34*35*36*37*43*50*51*53*54*56*57*65*71*72*73*74*75*76*78*79*80*87*93*94*96*97*98*100*101*108*114*115*116*117*119*120*121*122*123*124*125*132*138*139*140*141*142*144*145*146*153*154*160*161*162*164*165*166*167*168*169*176'''
+    idList = ids184.split('*')
 
-    idList = ids.split('*')
+    pagesList = keys184.split('*')
 
-    pagesList = keys.split('*')
+    def getKeys(self, book):
 
-    def getKeys(self):
-        for i in range(0, len(self.pagesList)):
-            localList = self.pagesList[i].split('^')
-            for j in range(0, len(localList)):
-                newLocal = ''
-                if localList[j].find('|'):
-                    newLocal = localList[j].split('|')[0]
-                    localList[j] = newLocal
-            self.pagesList[i] = ('^').join(localList)
+        if book == "大二下":
+            idList = self.ids184.split('*')
+            pagesList = self.keys184.split('*')
+            for i in range(0, len(pagesList)):
+                localList = pagesList[i].split('^')
+                for j in range(0, len(localList)):
+                    newLocal = ''
+                    if localList[j].find('|'):
+                        newLocal = localList[j].split('|')[0]
+                        localList[j] = newLocal
+                pagesList[i] = ('^').join(localList)
 
-        return zip(self.idList,self.pagesList)
+            return zip(idList, pagesList)
 
-    def testKeys(self):
-        for i in range(0, len(self.pagesList)):
-            localList = self.pagesList[i].split('^')
-            for j in range(0, len(localList)):
-                newLocal = ''
-                if localList[j].find('|'):
-                    newLocal = localList[j].split('|')[0]
-                    localList[j] = newLocal
-            self.pagesList[i] = ('^').join(localList)
+        elif book == "大一下":
+            idList = self.ids182.split('*')
+            pagesList = self.keys182.split('*')
+            for i in range(0, len(pagesList)):
+                localList = pagesList[i].split('^')
+                for j in range(0, len(localList)):
+                    newLocal = ''
+                    if localList[j].find('|'):
+                        newLocal = localList[j].split('|')[0]
+                        localList[j] = newLocal
+                pagesList[i] = ('^').join(localList)
 
-            print(str(i) + ' ' + self.idList[i] + ' -> ' + self.pagesList[i])
+            return zip(idList, pagesList)
 
+    def getLength(self, book):
+        if book == "大二下":
+            return len(self.keys184.split('*'))
+        elif book == "大一下":
+            return len(self.keys182.split('*'))
 
-a = keyGenerator()
-a.getKeys()
+    # def testKeys(self):
+    #     for i in range(0, len(self.pagesList)):
+    #         localList = self.pagesList[i].split('^')
+    #         for j in range(0, len(localList)):
+    #             newLocal = ''
+    #             if localList[j].find('|'):
+    #                 newLocal = localList[j].split('|')[0]
+    #                 localList[j] = newLocal
+    #         self.pagesList[i] = ('^').join(localList)
+    #
+    #         print(str(i) + ' ' + self.idList[i] + ' -> ' + self.pagesList[i])
+
